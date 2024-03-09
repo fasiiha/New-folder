0b89ee2454478570cab98b869770c8876eefc27c
Fasiha Arshad <fasihaa.arshad@gmail.com>
2024-03-09 17:59:10 +0500

    css

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
-import MainDashboard from "pages/MainDashboard";
-import Transaction from "pages/Transaction";
-import Accounts from "pages/Accounts";
-import Investments from "pages/Investments";
-import CreditCards from "pages/CreditCards";
+import MainDashboard from "pages/MainDashboard/MainDashboard";
+import Transaction from "pages/Transaction/Transaction";
+import Accounts from "pages/Accounts/Accounts";
+import Investments from "pages/Investments/Investments";
+import CreditCards from "pages/CreditCards/CreditCards";
 import Layout from "./layout";
-import LoanPage from "pages/Loan";
-import ServicesPage from "pages/Services";
-import SettingEditProfilePage from "pages/SettingEditProfile";
+import LoanPage from "pages/Loan/Loan";
+import ServicesPage from "pages/Services/Services";
+import SettingEditProfilePage from "pages/SettingEditProfile/SettingEditProfile";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
